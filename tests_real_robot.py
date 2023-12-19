7cc61b1fbed1561f929a6298f3b01175b1514dc8
Ningyuan Tian <mtian@violin.is.localnet>
2023-12-19 17:34:15 +0100

    modified default mirror script

--- tests_real_robot.py
+++ tests_real_robot.py
@@ -3,7 +3,7 @@ import o80
 import time
 
 segment_id = "real_robot"
-frontend = o80_pam.frontend(segment_id)
+frontend = o80_pam.FrontEnd(segment_id)
 
 ###
 ###
@@ -11,16 +11,16 @@ frontend = o80_pam.frontend(segment_id)
 
 # Test the pressure limits of joints.
 
-p_high = (30000,30000,30000,30000)
-p_low = (0,0,0,0)
+p_ago = (20000,19000,17000,17000)
+p_antago = (20000,20000,17000,17000)
 duration = o80.Duration_us.seconds(4)
 
-frontend.add_command(p_high, p_high, duration, o80.Mode.QUEUE)
+frontend.add_command(p_ago, p_antago, duration, o80.Mode.QUEUE)
 frontend.pulse()
 time.sleep(5)
 print("reached state robot under high pressure command: {}".format(frontend.latest()))
 
-frontend.add_command(p_low, p_low, duration, o80.Mode.QUEUE)
-frontend.pulse()
-time.sleep(5)
-print("reached state robot under low pressure command: {}".format(frontend.latest()))
+# frontend.add_command(p_low, p_low, duration, o80.Mode.QUEUE)
+# frontend.pulse()
+# time.sleep(5)
+# print("reached state robot under low pressure command: {}".format(frontend.latest()))
