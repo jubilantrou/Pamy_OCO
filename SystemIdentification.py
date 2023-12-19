2095e8162ac51abd106ca5a5cca13e58057ea8d2
Ningyuan Tian <mtian@violin.is.localnet>
2023-12-19 17:32:03 +0100

    added data and codes for mirroring test

--- SystemIdentification.py
+++ SystemIdentification.py
@@ -5,10 +5,11 @@ import numpy as np
 import o80
 import os
 from get_handle import get_handle
-# %% constant
-anchor_ago_list        = [19300,17500,17100,13000] #np.array([17500, 18500, 16000, 15000])
-anchor_ant_list        = [19900,22000,16900,13000] #np.array([17500, 18500, 16000, 15000])
-dof                    = 1 # which dof to excite
+import o80_pam
+# %% constant: anchor lists, dof, amp_list,
+anchor_ago_list        = [20000,19000,17000,17000] #np.array([17500, 18500, 16000, 15000])
+anchor_ant_list        = [20000,20000,17000,17000] #np.array([17500, 18500, 16000, 15000])
+dof                    = 3 # which dof to excite
 fs                     = 100
 frequency              = 500.0  # frequency of the backend 
 period                 = 1.0/frequency  # period of backend
@@ -16,10 +17,12 @@ duration_per_command   = 1 / fs  # period of frontend
 iterations_per_command = int(duration_per_command/period)  # sychronize the frontend and the backend
 anchor_ago             = anchor_ago_list[dof]
 anchor_ant             = anchor_ant_list[dof]
-amp_list               = [2.5] #[3.5,2.5]  # amp_u * 1000
+amp_list               = [2.0] #[3.5,2.0,3.5,2.0]  # amp_u * 1000
 # %% connect to the simulation and initilize the posture
-handle           = get_handle(mujoco_id='SI',mode='pressure',generation='second')
-frontend         = handle.frontends["robot"]
+# handle           = get_handle(mujoco_id='SI',mode='pressure',generation='second')
+# frontend         = handle.frontends["robot"]
+segment_id = "real_robot"
+frontend = o80_pam.FrontEnd(segment_id)
 duration         = o80.Duration_us.seconds(2)
 frontend.add_command(anchor_ago_list, anchor_ant_list,
                      duration,
@@ -28,7 +31,7 @@ frontend.pulse_and_wait()
 # %%
 p         = 10  # how many periods
 file_path = '/home/mtian/Pamy_OCO/excitation signals/'
-f_input   = file_path + 'excitation_10hz_0.csv'
+f_input   = file_path + 'excitation_2hz_0.csv' # two set signals
 u         = np.loadtxt(f_input, delimiter=',')
 [m, N]    = u.shape
 u         = np.tile(u, p).flatten()
@@ -44,7 +47,8 @@ for amp in amp_list:
     frontend.pulse_and_wait()
 
     u_temp = u * amp  #change the amplitude of the signal
-    f_output = file_path + "response_pamy2_dof_" + str(dof) + '_amp_' + str(amp) + ".txt"
+    f_output = file_path + "mirroring_real_dof_" + str(dof) + ".txt"
+    # f_output = file_path + "response_pamy2_real_dof_" + str(dof) + '_amp_' + str(amp) + 'excitation_2_hz_' + ".txt"
     # initilization
     position = np.array([])
     velocity = np.array([])
@@ -53,13 +57,15 @@ for amp in amp_list:
     des_pressure_ant = np.array([])
     des_pressure_ago = np.array([])
     t_stamp = np.array([])
+    i_stamp = np.array([]) # for mirroring
 
     ref_iteration   = frontend.latest().get_iteration()  # read the current iteration
     iteration_begin = ref_iteration + 1000  # set the iteration when beginning
     iteration       = iteration_begin
     
     print("begin to excite the mujoco...")
-    for i in range(0, len(u_temp)):
+    print(len(u_temp))
+    for i in range(90000, len(u_temp)):
         diff = int( u_temp[i] )
         
         # hardware paper as ref
@@ -98,6 +104,7 @@ for amp in amp_list:
         velocity = np.append(velocity, obs_velocity[dof])
 
         t_stamp = np.append(t_stamp, observation.get_time_stamp() * 1e-9)
+        i_stamp = np.append(i_stamp, observation.get_iteration())
 
         iteration += iterations_per_command
 
@@ -113,20 +120,28 @@ for amp in amp_list:
     print("number of simulation:",len(t_stamp))
     print("desired number:",len(t_stamp_u))
 
+    # print("begin to write data to the file...")
+    # f = open(f_output, 'w')
+    # f.write(str(p))
+    # f.write("\n")
+    # f.write(str(m))
+    # f.write("\n")
+    # f.write(str(N))
+    # f.write("\n")
+    # np.savetxt(f, t_stamp,          fmt='%.5f')
+    # np.savetxt(f, u_temp,           fmt='%.8f')
+    # np.savetxt(f, des_pressure_ago, fmt='%.5f')
+    # np.savetxt(f, des_pressure_ant, fmt='%.5f')
+    # np.savetxt(f, obs_pressure_ago, fmt='%.5f')
+    # np.savetxt(f, obs_pressure_ant, fmt='%.5f')
+    # np.savetxt(f, position,         fmt='%.8f')
+    # f.close()
+    # print('...completed')
+
     print("begin to write data to the file...")
     f = open(f_output, 'w')
-    f.write(str(p))
-    f.write("\n")
-    f.write(str(m))
-    f.write("\n")
-    f.write(str(N))
-    f.write("\n")
+    np.savetxt(f, i_stamp,          fmt='%.1f')
     np.savetxt(f, t_stamp,          fmt='%.5f')
-    np.savetxt(f, u_temp,           fmt='%.8f')
-    np.savetxt(f, des_pressure_ago, fmt='%.5f')
-    np.savetxt(f, des_pressure_ant, fmt='%.5f')
-    np.savetxt(f, obs_pressure_ago, fmt='%.5f')
-    np.savetxt(f, obs_pressure_ant, fmt='%.5f')
     np.savetxt(f, position,         fmt='%.8f')
     f.close()
     print('...completed')
